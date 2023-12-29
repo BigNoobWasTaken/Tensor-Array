48ed9fcbd86eaae2768022728ff610b9e07b5de6
Noob <63889503+BigNoobWasTaken@users.noreply.github.com>
2023-12-29 17:02:23 +0700

    update transformer

--- TensorCore/attention.hh
+++ TensorCore/attention.hh
@@ -7,18 +7,19 @@ namespace tensor_array
 {
     namespace layers
     {
-        value::Tensor CUDA_ML_API scaled_dot_product_attention(const value::Tensor& q, const value::Tensor& k, const value::Tensor& v);
+        value::Tensor CUDA_ML_API scaled_dot_product_attention(const value::Tensor&, const value::Tensor&, const value::Tensor&, const value::Tensor& = value::Tensor());
 
         class CUDA_ML_API MultiHeadAttentionImpl final :
             public LayerImpl,
-            public CalculateStruct<value::Tensor, const value::Tensor&, const value::Tensor&, const value::Tensor&>
+            public CalculateStruct<value::Tensor, const value::Tensor&, const value::Tensor&, const value::Tensor&, const value::Tensor&>
         {
         private:
             const unsigned int d_model, n_head;
             Linear w_q, w_k, w_v, w_o;
         public:
             MultiHeadAttentionImpl(unsigned int, unsigned int);
-            value::Tensor calculate(const value::Tensor&, const value::Tensor&, const value::Tensor&) override;
+            void init_value(const value::Tensor&, const value::Tensor&, const value::Tensor&, const value::Tensor& = value::Tensor()) override;
+            value::Tensor calculate(const value::Tensor&, const value::Tensor&, const value::Tensor&, const value::Tensor& = value::Tensor()) override;
         };
 
         using MultiHeadAttention = LayerHolder<MultiHeadAttentionImpl>;
--- TensorCore/transformer.cc
+++ TensorCore/transformer.cc
@@ -5,15 +5,19 @@ namespace tensor_array
 {
 	namespace layers
 	{
-		TransformerEncoderImpl::TransformerEncoderImpl(unsigned int d_model, unsigned int n_head) :
+		TransformerEncoderImpl::TransformerEncoderImpl(unsigned int d_model, unsigned int n_head, unsigned int ff_size) :
 			multihead_attn(d_model, n_head),
 			feed_forward
 			{
-				Linear(4 * d_model),
+				Linear(ff_size),
 				Activation(&ReLU),
 				Linear(d_model)
 			}
 		{
+			this->map_layer.insert(std::make_pair("MultiheadAttn", multihead_attn.get_shared()));
+			this->map_layer.insert(std::make_pair("FeedForward", feed_forward.get_shared()));
+			this->map_layer.insert(std::make_pair("LayerNorm1", layer_norm_1.get_shared()));
+			this->map_layer.insert(std::make_pair("LayerNorm2", layer_norm_2.get_shared()));
 		}
 		value::Tensor TransformerEncoderImpl::calculate(const value::Tensor& input)
 		{
@@ -21,6 +25,55 @@ namespace tensor_array
 			attn_output = this->layer_norm_1(input + attn_output);
 			auto ff_output = this->feed_forward(attn_output);
 			return this->layer_norm_2(attn_output + ff_output);
+		}
+		TransformerDecoderImpl::TransformerDecoderImpl(unsigned int d_model, unsigned int n_head, unsigned int ff_size) :
+			masked_multihead_attn(d_model, n_head),
+			multihead_attn(d_model, n_head),
+			feed_forward
+			{
+				Linear(ff_size),
+				Activation(&ReLU),
+				Linear(d_model)
+			}
+		{
+			this->map_layer.insert(std::make_pair("MaskedMultiheadAttn", masked_multihead_attn.get_shared()));
+			this->map_layer.insert(std::make_pair("MultiheadAttn", multihead_attn.get_shared()));
+			this->map_layer.insert(std::make_pair("FeedForward", feed_forward.get_shared()));
+			this->map_layer.insert(std::make_pair("LayerNorm1", layer_norm_1.get_shared()));
+			this->map_layer.insert(std::make_pair("LayerNorm2", layer_norm_2.get_shared()));
+			this->map_layer.insert(std::make_pair("LayerNorm3", layer_norm_3.get_shared()));
+		}
+		value::Tensor TransformerDecoderImpl::calculate(const value::Tensor& tgt, const value::Tensor& memory_encode, const value::Tensor& tgt_mask)
+		{
+			auto attn_output = this->masked_multihead_attn(tgt, tgt, tgt, tgt_mask);
+			attn_output = this->layer_norm_1(tgt + attn_output);
+
+			auto attn_output_2 = this->multihead_attn(attn_output, memory_encode, memory_encode);
+			attn_output = this->layer_norm_2(attn_output + attn_output_2);
+
+			auto ff_output = this->feed_forward(attn_output_2);
+			ff_output = this->layer_norm_3(attn_output_2 + ff_output);
+			return ff_output;
+		}
+		TransformerImpl::TransformerImpl(unsigned int d_model, unsigned int n_head, unsigned int ff_size, unsigned int num_layers):
+			fc(ff_size),
+			encoder_blocks()
+		{
+			for (size_t i = 0; i < num_layers; i++)
+			{
+				encoder_blocks->insert(TransformerEncoder(d_model, n_head, ff_size));
+				TransformerDecoder temp_decoder(d_model, n_head, ff_size);
+				decoder_blocks.push_back(std::move(temp_decoder));
+			}
+		}
+		value::Tensor TransformerImpl::calculate(const value::Tensor& src, const value::Tensor& tgt, const value::Tensor& tgt_mask)
+		{
+			value::Tensor src_e = src;
+			value::Tensor tgt_e = tgt;
+			src_e = this->encoder_blocks(src_e);
+			for (auto& it : this->decoder_blocks)
+				tgt_e = it(tgt_e, src_e, tgt_mask);
+			return SoftMax(this->fc(tgt_e), 0);
 		}
 	}
 }
